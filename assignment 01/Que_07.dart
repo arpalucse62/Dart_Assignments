464260a72171eecbec80bd3dfca2923449c339a2
Arpa Das <arpalucse62@gmail.com>
2025-10-11 22:46:53 +0600

    Create Que_07.dart

---
+++ assignment 01/Que_07.dart
@@ -0,0 +1,14 @@
+void main() {
+  int dividend = 27;
+  int divisor = 5;
+
+ 
+  int quotient = dividend ~/ divisor;
+
+  
+  int remainder = dividend % divisor;
+
+  print("Dividend: $dividend, Divisor: $divisor");
+  print("Quotient (27 / 5): $quotient");
+  print("Remainder (27 % 5): $remainder");
+}
