db8276832efb542890baecafac71231f7ee17087
Arpa Das <arpalucse62@gmail.com>
2025-10-11 22:44:41 +0600

    Create Que_04.dart

---
+++ assignment 01/Que_04.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  double p = 1000.0;
+ 
+  double t = 2.0;
+  
+  double r = 5.0;
+
+  
+  double simpleInterest = (p * t * r) / 100;
+
+  print("Principle (P): \$$p");
+  print("Time (T): $t years");
+  print("Rate (R): $r%");
+  print("Simple Interest (I): \$$simpleInterest");
+}
