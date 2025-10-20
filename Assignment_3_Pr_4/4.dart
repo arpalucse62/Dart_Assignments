92c96fa512e75f96da2d10dcce5fe76b2f88d152
Arpa Das <arpalucse62@gmail.com>
2025-10-20 18:47:51 +0600

    qs_4

---
+++ Assignment_3_Pr_4/4.dart
@@ -0,0 +1,13 @@
+void task4() {
+  List<String> days = []; 
+  
+  days.add('Sun');
+  days.add('Mon');
+  days.add('Tue');
+  days.add('Wed');
+  days.add('Thu');
+  days.add('Fri');
+  days.add('Sat');
+  
+  print('4. Days List: $days');
+}
